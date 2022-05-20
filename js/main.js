45fc06aad7b66b0ea015fd1532cc30149fc8b449
Anton-Osip <63441692+Anton-Osip@users.noreply.github.com>
2022-05-20 17:35:59 +0300

    end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(function(){
-
-});
+$(function () {
+	$('.design-slider').slick({
+		dots: false,
+		infinite: true,
+		slidesToShow: 4,
+		variableWidth: true,
+		prevArrow:
+			'<img src="./images/arrow-left.svg" alt="arrow left" class="arrow arrow-left">',
+		nextArrow:
+			'<img src="./images/arrow-right.svg" alt="arrow right" class="arrow arrow-right">',
+		responsive: [
+			{
+				breakpoint: 361,
+				settings: {
+					variableWidth: false,
+					slidesToShow: 1,
+				},
+			},
+		],
+	})
+})
